79c33afffd8e68b6b7b5e2729524364162ad49ad
Michael Yu <michael.yu2016@gmail.com>
2017-09-24 14:28:24 -0700

    added images

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,20 @@ $(document).ready(() => {
     $(".navbar-collapse").toggle('in');
   });
   
+  //SMOOTH SCROLL
+  $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+          'scrollTop': $target.offset().top
+      }, 600, 'swing', function () {
+          window.location.hash = target;
+      });
+  });
+
   $( "#emailForm" ).submit(function( event ) {
     event.preventDefault()
     const email = $("[name='emailform']").val()
